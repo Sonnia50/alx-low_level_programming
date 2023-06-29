59610765bc1c10c6b61171c1db35baa6d8dd2559
Sonnia50 <diannesonia46@gmail.com>
2023-06-29 14:57:16 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 'a'; d <= 'z'; d++)
+		putchar(ch);
+	for (d = 'A'; d <= 'Z'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
